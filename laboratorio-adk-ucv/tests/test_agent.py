dc190eee3d0e63130c9a7741146864f604897a6b
Luciana Palomino <lpalominoti17@ucvvirtual.edu.pe>
2026-05-30 19:24:24 -0500

    Resolución del laboratorio completado al 100%

--- laboratorio-adk-ucv/tests/test_agent.py
+++ laboratorio-adk-ucv/tests/test_agent.py
@@ -1,13 +1,24 @@
-from agente_ucv.agent import explicar_concepto
+import pytest
+
+from agente_ucv.agent import (explicar_concepto, calcular_promedio)
 
 
 def test_api():
     resultado = explicar_concepto("api")
-
     assert resultado["status"] == "success"
 
 
-def test_concepto_inexistente():
-    resultado = explicar_concepto("docker")
+def test_concepto_no_encontrado():
 
+    resultado = explicar_concepto("robotica")
     assert resultado["status"] == "not_found"
+    
+def test_calcular_promedio():
+
+    resultado = calcular_promedio(18, 16, 20)
+    assert resultado["status"] == "success"
+    assert resultado["promedio"] == pytest.approx(18.0)
+
+
+
+
